e870847c2a28e270e248b39524ca4137c8b1797d
Fedor <fedor.avdeev@gmail.com>
2016-07-30 19:22:04 +0300

    create splash in app Signed-off-by: Fedor <fedor.avdeev@gmail.com>

--- app/src/main/java/fm/ua/afv/myreminder/MainActivity.java
+++ app/src/main/java/fm/ua/afv/myreminder/MainActivity.java
@@ -1,13 +1,56 @@
 package fm.ua.afv.myreminder;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    FragmentManager fragmentManager;
+    PreferenceHelper preferenceHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        preferenceHelper.getInstance().init(getApplicationContext());
+        preferenceHelper = PreferenceHelper.getInstance();
+
+        fragmentManager = getSupportFragmentManager();
+        runSplash();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem splashItem = menu.findItem(R.id.action_splash);
+        splashItem.setChecked(preferenceHelper.getBoolean(PreferenceHelper.SPLASH_IS_INVISIBLE));
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.action_splash) {
+            item.setChecked(!item.isChecked());
+            preferenceHelper.putBoolean(PreferenceHelper.SPLASH_IS_INVISIBLE, item.isChecked());
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    
+    public void runSplash(){
+        if (!preferenceHelper.getBoolean(PreferenceHelper.SPLASH_IS_INVISIBLE)) {
+            SplashFragment splashFragment = new SplashFragment();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.container, splashFragment)
+                    .addToBackStack(null)
+                    .commit();
+        }
     }
 }
