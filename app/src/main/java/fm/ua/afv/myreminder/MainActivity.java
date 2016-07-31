e296c0a3b82af6912da215fd002da929e7333caf
Fedor <fedor.avdeev@gmail.com>
2016-07-31 09:53:59 +0300

    new splash view.
    Signed-off-by: Fedor <fedor.avdeev@gmail.com>

--- app/src/main/java/fm/ua/afv/myreminder/MainActivity.java
+++ app/src/main/java/fm/ua/afv/myreminder/MainActivity.java
@@ -3,11 +3,13 @@ package fm.ua.afv.myreminder;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
     FragmentManager fragmentManager;
     PreferenceHelper preferenceHelper;
 
@@ -16,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar); // Attaching the layout to the toolbar object
+        setSupportActionBar(toolbar);
 
         preferenceHelper.getInstance().init(getApplicationContext());
         preferenceHelper = PreferenceHelper.getInstance();
@@ -27,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        MenuItem splashItem = menu.findItem(R.id.action_splash);
+        MenuItem splashItem = menu.findItem(R.id.action_settings);
         splashItem.setChecked(preferenceHelper.getBoolean(PreferenceHelper.SPLASH_IS_INVISIBLE));
         return true;
     }
@@ -43,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-    
+
     public void runSplash(){
         if (!preferenceHelper.getBoolean(PreferenceHelper.SPLASH_IS_INVISIBLE)) {
             SplashFragment splashFragment = new SplashFragment();
